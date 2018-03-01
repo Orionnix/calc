a1b9e78306c89908c5a7b1c658d34a4c1aeae12e
Daniel Trujillo García <lightinad@gmail.com>
2018-03-01 14:26:54 +0000

    Modificación del color, distribución y border, no se me centra en la pagina

---
+++ js/jscalc.js
@@ -0,0 +1,13 @@
+function vaciar () {
+  document.getElementById("n1").value = "";
+}
+
+function cuadrado() {
+  var num = document.getElementById("n1");
+  num.value = num.value * num.value;
+}
+
+function inverso() {
+  var num = document.getElementById("n1");
+  num.value = 1/num.value;
+}
